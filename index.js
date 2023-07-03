abeb0a3ca16c01651bc4d240887280643c6128e3
kharoteswati <kharoteswati31@gmail.com>
2023-07-03 13:16:26 +0530

    ok

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ server.use(middlewares)
 server.use(router)
 server.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
-    res.header("Access-Control-Allow-Headers", "*")
+    res.header("Access-Control-Allow-Headers", "*");
+    next()
 })
 
 
