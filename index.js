142725f51884eafdcb9f2008806ec7a0c0a4afbb
kharoteswati <kharoteswati31@gmail.com>
2023-07-03 13:09:44 +0530

    change

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
+server.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+})
+
+
 server.listen(process.env.PORT || 3000, () => {
     console.log('JSON Server is running')
 })
