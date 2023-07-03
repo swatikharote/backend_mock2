0aec85c5a2fa62c99ee2383d7658276ff1d593bc
kharoteswati <kharoteswati31@gmail.com>
2023-07-03 13:35:16 +0530

    new

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const router = jsonServer.router("db.json")
 const middlewares = jsonServer.defaults({ noCors: true })
 
 server.use(middlewares)
-server.use(router)
+
 server.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*")
     res.setHeader("Access-Control-Allow-Headers", "*");
     next()
 })
-
+server.use(router)
 
 server.listen(process.env.PORT || 3000, () => {
     console.log('JSON Server is running')
